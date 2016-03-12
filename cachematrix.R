2b229f4d7a9ed390f671705f87653aba308d6272
Fernando Carlini Guimarães <nando.dsg@gmail.com>
2016-03-12 15:53:24 -0300

    Detailed comments and changed variable names from m to i and mean to inv

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,19 @@
 ## This code writes funcionts that are able to cache potentially time-consuming
 ## computations, in this case, returning the inverse of a matrix using solve().
 
-## Function the stores a list of functions to a given object.
+## Function that stores a list of functions to a given object. Allows user to
+## set and get to natrux to retrieve it's inverse, and also allows user to set
+## and get inversed matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        i <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(mean) m <<- mean
-        getinverse <- function() m
+        setinverse <- function(inv) i <<- inv
+        getinverse <- function() i
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -26,13 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
+        i <- x$getinverse()
+        if(!is.null(i)) {
                 message("getting cached data")
-                return(m)
+                return(i)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
